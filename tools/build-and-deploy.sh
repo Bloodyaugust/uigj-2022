f2d35dedeb5c7ea9e05a948c231ec0a7f0263795
Bloodyaugust <greysonrichey@gmail.com>
2022-10-14 20:13:22 -0600

    Edit for uigj-2022

--- tools/build-and-deploy.sh
+++ tools/build-and-deploy.sh
@@ -18,24 +18,24 @@ mkdir build/win/
 
 echo "EXPORTING FOR LINUX"
 echo "-----------------------------"
-godot --export "Linux/X11" build/linux/godot-template.x86_64 -v
+godot --export "Linux/X11" build/linux/uigj-2022.x86_64 -v
 # echo "EXPORTING FOR OSX"
 # echo "-----------------------------"
-# godot --export "Mac OSX" build/osx/godot-template.dmg -v
+# godot --export "Mac OSX" build/osx/uigj-2022.dmg -v
 echo "EXPORTING FOR WINDOZE"
 echo "-----------------------------"
-godot --export-debug "Windows Desktop" build/win/godot-template.exe -v
+godot --export-debug "Windows Desktop" build/win/uigj-2022.exe -v
 echo "-----------------------------"
 
 # echo "CHANGING FILETYPE AND CHMOD EXECUTABLE FOR OSX"
 # echo "-----------------------------"
 # cd build/osx/
-# mv godot-template.dmg godot-template-osx-alpha.zip
-# unzip godot-template-osx-alpha.zip
-# rm godot-template-osx-alpha.zip
-# chmod +x godot-template.app/Contents/MacOS/godot-template
-# zip -r godot-template-osx-alpha.zip godot-template.app
-# rm -rf godot-template.app
+# mv uigj-2022.dmg uigj-2022-osx-alpha.zip
+# unzip uigj-2022-osx-alpha.zip
+# rm uigj-2022-osx-alpha.zip
+# chmod +x uigj-2022.app/Contents/MacOS/uigj-2022
+# zip -r uigj-2022-osx-alpha.zip uigj-2022.app
+# rm -rf uigj-2022.app
 # cd ../../
 
 ls -al
@@ -47,15 +47,15 @@ ls -al build/win/
 echo "ZIPPING FOR WINDOZE"
 echo "-----------------------------"
 cd build/win/
-zip -r godot-template-win-alpha.zip godot-template.exe godot-template.pck
-rm -r godot-template.exe godot-template.pck
+zip -r uigj-2022-win-alpha.zip uigj-2022.exe uigj-2022.pck
+rm -r uigj-2022.exe uigj-2022.pck
 cd ../../
 
 echo "ZIPPING FOR LINUX"
 echo "-----------------------------"
 cd build/linux/
-zip -r godot-template-linux-alpha.zip godot-template.x86_64 godot-template.pck
-rm -r godot-template.x86_64 godot-template.pck
+zip -r uigj-2022-linux-alpha.zip uigj-2022.x86_64 uigj-2022.pck
+rm -r uigj-2022.x86_64 uigj-2022.pck
 cd ../../
 
 echo "Logging in to Butler"
@@ -64,6 +64,6 @@ butler login
 
 echo "Pushing builds with Butler"
 echo "-----------------------------"
-butler push build/linux/ synsugarstudio/godot-template:linux-alpha
-# butler push build/osx/ synsugarstudio/godot-template:osx-alpha
-butler push build/win/ synsugarstudio/godot-template:win-alpha
+butler push build/linux/ synsugarstudio/uigj-2022:linux-alpha
+# butler push build/osx/ synsugarstudio/uigj-2022:osx-alpha
+butler push build/win/ synsugarstudio/uigj-2022:win-alpha
