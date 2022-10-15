03612b5a17acaa7fb1b59d3c05ff100b6f02c29a
Greyson Richey <greysonrichey@gmail.com>
2022-10-15 15:01:10 -0600

    Add basic Room actor
    - Add Easy room

---
+++ actors/Room.gd
@@ -0,0 +1,8 @@
+extends TileMap
+
+export var scene_data: Resource
+
+#var room_data: RoomData
+
+func _enter_tree() -> void:
+  pass
---
+++ data/room-data.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name RoomData
+
+export var difficulty: float
