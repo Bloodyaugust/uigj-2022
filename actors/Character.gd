9d43fa7391429301c2a5d93314bd06544f769b4c
Greyson Richey <greysonrichey@gmail.com>
2022-10-15 20:09:15 -0600

    Start implementation of weapons

--- actors/Character.gd
+++ actors/Character.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
+const WEAPON_ORBIT_RADIUS: float = 12.0
+
 export var scene_data: Resource
 
+var aim_direction: Vector2 = Vector2.ZERO
 var data: CharacterData
 var direction: Vector2 = Vector2.ZERO
 var health: float
 
 onready var _animated_sprite: AnimatedSprite = $"%AnimatedSprite"
+onready var _weapon: Sprite = $"%Weapon"
 
 var _time_to_attack: float = 0.0
 
@@ -28,6 +32,9 @@ func damage(amount: float) -> void:
 
 func _process(delta: float) -> void:
   _time_to_attack = clamp(_time_to_attack - delta, 0.0, data.attack_interval)
+
+  _weapon.global_position = global_position + (aim_direction * WEAPON_ORBIT_RADIUS)
+  _weapon.global_rotation = _weapon.global_position.angle_to_point(global_position)
 
 func _physics_process(delta: float) -> void:
   if direction.length_squared() > 0:
--- scripts/controllers/PlayerController.gd
+++ scripts/controllers/PlayerController.gd
@@ -8,6 +8,7 @@ func _process(delta):
   _movement_direction += Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized()
 
   character.direction = _movement_direction
+  character.aim_direction = global_position.direction_to(get_global_mouse_position())
 
   if Input.is_action_just_pressed("attack"):
     character.attack()
