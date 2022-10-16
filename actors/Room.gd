4e5a37754fb256b7bb382a02f4b63a26c72cf3a9
Bloodyaugust <greysonrichey@gmail.com>
2022-10-16 12:40:09 -0600

    Set up rooms for going to the next room
    - Add EnemyWitch to the rotation of spawns

--- actors/Room.gd
+++ actors/Room.gd
@@ -6,7 +6,7 @@ const CHARACTER_SCENES: Array = [
  ]
 const ENEMY_SCENES: Array = [
   preload("res://actors/enemies/Basic.tscn"),
-  preload("res://actors/enemies/EnemyMage.tscn"),
+  preload("res://actors/enemies/EnemyWitch.tscn"),
  ]
 const PLAYER_SCRIPT: Script = preload("res://scripts/controllers/PlayerController.gd")
 
@@ -14,6 +14,7 @@ export var scene_data: Resource
 
 onready var _enemy_container: Node = get_tree().get_root().find_node("EnemyContainer", true, false)
 onready var _enemy_spawns: Node2D = $"%EnemySpawns"
+onready var _exit_room: Area2D = $"%ExitRoom"
 onready var _player_container: Node = get_tree().get_root().find_node("PlayerContainer", true, false)
 onready var _player_spawn: Node2D = $"%PlayerSpawn"
 
@@ -22,8 +23,14 @@ var room_data: RoomData
 func _enter_tree() -> void:
   room_data = scene_data
 
+func _on_exit_room_body_enetered(body: Node) -> void:
+  # TODO: Get the next room, transition, do the stuff
+  print("room exited")
+
 func _ready() -> void:
   var _enemy_spawn_points: Array = _enemy_spawns.get_children()
+
+  _exit_room.connect("body_entered", self, "_on_exit_room_body_enetered")
 
   for _i in range(room_data.difficulty):
     var _new_enemy: Node2D = ENEMY_SCENES[randi() % ENEMY_SCENES.size()].instance()
